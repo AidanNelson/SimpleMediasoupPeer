da562f32a25ad61f4d19d623b7a3db6285b61479
AidanNelson <AidanNelson@users.noreply.github.com>
2022-08-04 12:46:41 -0400

    add simple broadcast example

---
+++ examples/broadcaster/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const http = require('http');
+const SimpleMediasoupPeerServer = require("simple-mediasoup-peer-server");
+const io = require('socket.io')()
+
+
+const app = express()
+const server = http.createServer(app)
+
+io.listen(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+        credentials: true,
+    },
+})
+
+// serve the client-side files
+const distFolder = process.cwd() + '/public'
+console.log('Serving static files at ', distFolder)
+app.use(express.static(process.cwd() + '/public'))
+
+const port = 5000;
+server.listen(port)
+console.log(`Server listening on http://localhost:${port}`);
+
+// keep track of all clients here
+let clients = {};
+
+function setupSocketServer() {
+    io.on('connection', (socket) => {
+        console.log('User ' + socket.id + ' connected, there are ' + io.engine.clientsCount + ' clients connected')
+        
+        socket.emit('clients', Object.keys(clients));
+        socket.broadcast.emit('clientConnected', socket.id);
+        
+        // then add to our clients object
+        clients[socket.id] = {}; // store initial client state here
+        
+        socket.on('disconnect', () => {
+            delete clients[socket.id];
+            io.sockets.emit('clientDisconnected', socket.id);
+            console.log('client disconnected: ', socket.id);
+        })
+    });
+}
+
+
+function main() {
+    // the mediasoup manager object will set up additional event 
+    // handlers on the socket-io object
+    new SimpleMediasoupPeerServer(io); 
+    setupSocketServer();
+}
+
+main();
