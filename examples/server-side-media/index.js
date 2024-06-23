845fe25b086e0ab494f6d221eb2be2a48e814b22
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2024-06-23 12:17:03 -0400

    WIP mediarecorder to mediasoup

--- examples/server-side-media/index.js
+++ examples/server-side-media/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const http = require("http");
+const fs = require("fs");
 
 // uncomment one of the following lines to see all mediasoup's internal logging messages:
 // process.env.DEBUG = "mediasoup*" // show everything mediasoup related
@@ -19,7 +20,62 @@ const port = 5000;
 server.listen(port);
 console.log(`Server listening on http://localhost:${port}`);
 
-let smp = new SimpleMediasoupPeerServer();
-setTimeout(() => {
-  smp.playVideoToRoom({ file: "./test.mp4", roomId: "broadcastRoom123" });
-}, 1000);
+// We will use the socket.io library to manage Websocket connections
+const { Server } = require("socket.io");
+const io = new Server({ maxHttpBufferSize: 1e8 });
+io.listen(server);
+
+const peers = {};
+const playbackQueue = new Set();
+
+// Set up each socket connection
+io.on("connection", (socket) => {
+  console.log(
+    "Peer joined with ID",
+    socket.id,
+    ". There are " + io.engine.clientsCount + " peer(s) connected."
+  );
+
+  // add a new peer indexed by their socket id
+  peers[socket.id] = { recordings: [] };
+
+  socket.on("media-chunk", ({ chunk, uuid }) => {
+    console.log("got media chunk");
+    fs.appendFile(`recordings/${socket.id}-${uuid}.webm`, chunk, (err) => {
+      if (err) throw err;
+      console.log("Chunk received and saved");
+      if (!playbackQueue.has(uuid)){
+        playbackQueue.add(uuid);
+        smp.playVideoToRoom({ file: `recordings/${socket.id}-${uuid}.webm`, roomId: "broadcastRoom123" });
+      }
+    });
+  });
+
+  socket.on("recording-start", ({ uuid }) => {
+    console.log("recording started:", uuid);
+    peers[socket.id].recordings.push(uuid);
+  });
+
+  socket.on("recording-end", ({ uuid }) => {
+    console.log("recording ended:", uuid);
+    
+  });
+
+  // // handle disconnections
+  socket.on("disconnect", () => {
+    delete peers[socket.id];
+    // io.sockets.emit("peerDisconnection", socket.id);
+    console.log(
+      "Peer " +
+        socket.id +
+        " diconnected, there are " +
+        io.engine.clientsCount +
+        " peer(s) connected."
+    );
+  });
+});
+
+let smp = new SimpleMediasoupPeerServer({ io: io });
+// setTimeout(() => {
+//   smp.playVideoToRoom({ file: "./test.webm", roomId: "broadcastRoom123" });
+// }, 1000);
--- examples/server-side-media/public/index.js
+++ examples/server-side-media/public/index.js
@@ -1,4 +1,7 @@
-let socket;
+let socket = io('http://localhost:5000', {
+  path: "/socket.io/",
+  autoConnect: false
+})
 let mediasoupPeer;
 
 window.onload = () => {
@@ -9,8 +12,10 @@ window.onload = () => {
 };
 
 function init() {
-  mediasoupPeer = new SimpleMediasoupPeer();
+  mediasoupPeer = new SimpleMediasoupPeer({socket: socket});
+  socket.connect();
   mediasoupPeer.on("track", gotTrack);
+  
   mediasoupPeer.joinRoom("broadcastRoom123");
 }
 
@@ -25,9 +30,10 @@ function gotTrack({ track, peerId, label }) {
     console.log("Creating video element for client with ID: " + peerId);
     el = document.createElement("video");
     el.id = peerId + "_video";
-    el.autoplay = true;
-    el.muted = true;
     el.setAttribute("playsinline", true);
+    el.setAttribute("autoplay", true);
+    el.setAttribute("muted", true);
+    el.setAttribute("controls", true);
     document.body.appendChild(el);
   }
 
@@ -41,12 +47,15 @@ function gotTrack({ track, peerId, label }) {
     el.volume = 0; // this is here to avoid feedback while testing
   }
 
-  el.srcObject = null;
-  el.srcObject = new MediaStream([track]);
+
 
   el.onloadedmetadata = (e) => {
+    console.log('attempting to play media');
     el.play().catch((e) => {
       console.log("Play Error: " + e);
     });
   };
+
+  el.srcObject = null;
+  el.srcObject = new MediaStream([track]);
 }
